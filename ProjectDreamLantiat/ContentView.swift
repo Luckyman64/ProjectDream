018aedc19581bbb890273e2c294dccabe618f5ac
luckyman64 <clement.lantiat@gmail.com>
2022-11-30 13:58:54 +0100

    Initial Commit

---
+++ ProjectDreamLantiat/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ProjectDreamLantiat
+//
+//  Created by Clement Lantiat on 09/09/1401 AP.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ProjectDreamLantiat/ProjectDreamLantiatApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectDreamLantiatApp.swift
+//  ProjectDreamLantiat
+//
+//  Created by Clement Lantiat on 09/09/1401 AP.
+//
+
+import SwiftUI
+
+@main
+struct ProjectDreamLantiatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
